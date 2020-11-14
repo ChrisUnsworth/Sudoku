b03545ee612f7d4b5ba5e064bfc50d6bddfaef8d
ChrisUnsworth <chris.unsworth79@gmail.com>
2020-11-14 13:49:37 +0000

    Search State

---
+++ Sudoku/Numbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sudoku
+{
+    public enum Numbers
+    {
+        One = 1,
+        Two = 2,
+        Three = 4,
+        Four = 8,
+        Five = 16,
+        Six = 32,
+        Seven = 64,
+        Eight = 128,
+        Nine = 256,
+        Any = 511
+    }
+}
---
+++ Sudoku/SearchState.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sudoku
+{
+    public class SearchState
+    {
+        private int[,] _data;
+
+        public SearchState()
+        {
+            _data = new int[9, 9];
+
+            for (int i; i < 9; i++)
+            {
+                for (int j; j < 9; j++)
+                {
+                    _data[i, j] = (int)Numbers.Any;
+                }
+            }
+        }
+
+        public bool IsSolution => throw new NotImplementedException();
+
+        public int? this[int x, int y]
+        {
+            get 
+            {
+                var v = _data[x, y];
+                if (NumberOfSetBits(v) == 1)
+                {
+                    return (int)Math.Log2(v);
+                }
+
+                return null;
+            }
+
+            set
+            {
+                if (value.HasValue)
+                {
+                    _data[x, y] = (int)Math.Pow(2, value.Value);
+                }
+            }
+        }
+
+        public bool CanBe(int x, int y, int val)
+        {
+            var bitVal = (int)Math.Pow(2, val);
+            return (bitVal & _data[x, y]) == bitVal;
+        }
+
+        public void SetNot(int x, int y, int val)
+        {
+            var bitVal = (int)Math.Pow(2, val);
+            var bitNotVal = (int)Numbers.Any - bitVal;
+            _data[x, y] = _data[x, y] & bitNotVal;
+        }
+
+        private int NumberOfSetBits(int i)
+        {
+            i = i - ((i >> 1) & 0x55555555);
+            i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+            return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+        }
+    }
+}
